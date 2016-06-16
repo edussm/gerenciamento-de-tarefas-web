a242dc9ede72c06f4a1f925556502ee4af61e29e
Eduardo Santos <edu.ufu@gmail.com>
2016-06-15 21:25:13 -0300

    Update import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,5 @@
 insert into tarefa(descricao, status) values ('Tarefa 1', 'PENDENTE');
 insert into tarefa(descricao, status) values ('Tarefa 2', 'PENDENTE');
+insert into membro(nome, email) values ('Eduardo', 'eduardo@mail.com');
+insert into membro(nome, email) values ('Aluno', 'aluno@mail.com');
+insert into membro(nome, email) values ('Lilian', 'lilian@mail.com');
